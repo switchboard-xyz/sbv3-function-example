e5a4dca0b563a72e2d518a3270ffe0915f87e887
mgild <mgild@users.noreply.github.com>
2023-05-04 15:59:51 -0400

    more build setup

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 export DOCKER_BUILDKIT=1
-IMG_NAME=${function-base}
-docker build . -f Dockerfile.builder --tag "$IMG_NAME"
-id=$(docker run -it -d --rm "$IMG_NAME" bash)
-rm -rf bin/
-mkdir -p bin
-docker cp "$id":/app/app bin/app
-docker kill "$id"
 IMG_NAME=${1:-function}
-docker build . -f Dockerfile.function --tag "$IMG_NAME"
+docker build . -f Dockerfile --tag "$IMG_NAME"
+id=$(docker run -it -d --rm "$IMG_NAME" bash)
+mkdir -p out/
+docker cp "$id":/measurement.txt out/measurement.txt
+docker kill "$id"
+MEASUREMENT="$(cat out/measurement.txt)"
+echo "Measurement: ${MEASUREMENT}"
+docker images -f "reference=${IMG_NAME}" | grep -v "<none>"
