7b886841c6d38cdd9c02978eda1e7de3df6151c8
Mitch Gildenberg <mgild@umich.edu>
2023-06-13 16:45:02 +0000

    Secrets fetch example

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,20 @@ use rsa::{RsaPublicKey, RsaPrivateKey};
 use rsa::pkcs8::ToPublicKey;
 use rand::rngs::OsRng;
 use serde_json::{json, Value};
+use std::collections::HashMap;
+use rsa::PaddingScheme;
+use serde_json;
 
 const DEMO_PID: Pubkey = pubkey!("8kjszBCEgkzAsU6QySHSZvr9yFaboau2RnarCQFFvasS");
+
+#[allow(dead_code)]
+#[allow(non_snake_case)]
+#[derive(Debug, Deserialize)]
+struct Secrets {
+    mrEnclaves: Vec<String>,
+    permittedAdvisories: Vec<String>,
+    keys: HashMap<String, String>,
+}
 
 #[derive(Clone, AnchorSerialize, AnchorDeserialize, Debug, Default)]
 pub struct PingParams {
@@ -42,15 +54,14 @@ struct Ticker {
     volume: String,
 }
 
-#[tokio::main(worker_threads = 12)]
-async fn main() {
+async fn fetch_secrets(url: &str) -> Secrets {
     let mut os_rng = OsRng::default();
     let priv_key = RsaPrivateKey::new(&mut os_rng, 2048).unwrap();
     let pub_key = RsaPublicKey::from(&priv_key).to_public_key_der().unwrap();
     let pub_key: &[u8] = pub_key.as_ref();
     let secrets_quote = Sgx::gramine_generate_quote(pub_key).unwrap();
     let client = reqwest::Client::new();
-    let secrets = client.post("http://20.254.56.236:8080")
+    let res = client.post("http://20.254.56.236:8080")
         .json(&json!({
             "quote": &secrets_quote,
             "pubkey": pub_key,
@@ -58,6 +69,15 @@ async fn main() {
         .send()
         .await
         .unwrap();
+    let ciphertext = res.bytes().await.unwrap();
+    let padding = PaddingScheme::new_pkcs1v15_encrypt();
+    let secrets: Secrets = serde_json::from_slice(&priv_key.decrypt(padding, &ciphertext).unwrap()).unwrap();
+    secrets
+}
+
+#[tokio::main(worker_threads = 12)]
+async fn main() {
+    let secrets = fetch_secrets("http://20.254.56.236:8080").await;
 
     let symbols = ["BTCUSDC", "ETHUSDC", "SOLUSDT"];
 
